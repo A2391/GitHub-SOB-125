8cafcc51984ed198d8ca3fd69e82c5464e9f14f5
A2391 <ak2391@live.mdx.ac.uk>
2021-11-25 09:38:25 +0000

    MiniProject

---
+++ 100.py
@@ -0,0 +1,7 @@
+#MiniProject SOB 100 Q1
+
+n = int(input("Enter a number of n: "))
+for i in range(n):
+    print("* "*(i+1))
+for i in range(n-1):
+    print("* "*(n-i-1))
---
+++ 101.py
@@ -0,0 +1,10 @@
+#MiniProject SOB 101 Q3
+
+n = 5
+
+for i in range(3):
+
+    for i in range(n):
+        print("* "*(i+1))
+    for i in range(n-1):
+        print("* "*(n-i-1))
